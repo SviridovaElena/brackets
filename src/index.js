f029dfd95fa323306ac261504b99b0a1474aeb88
SviridovaElena <sviridova.elena.konstantinovna@gmail.com>
2022-02-21 12:44:09 +0200

    feat: added solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  let pairElements = Object.fromEntries(bracketsConfig);
+  let stack = [];
+
+  for(let i = 0; i < str.length; i++) {
+    let curSymbol = str[i];
+
+    if(pairElements[curSymbol] === curSymbol) {
+      if(stack[stack.length - 1] === curSymbol) {
+        stack.pop();
+      } else {
+        stack.push(curSymbol);
+      }
+    } else if(pairElements[curSymbol]) {
+      stack.push(curSymbol);
+    } else if(pairElements[stack[stack.length - 1]] === curSymbol) {
+      stack.pop();
+    } else {
+      return false;
+    }
+  }
+
+  return stack.length === 0;
 }
+
